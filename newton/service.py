e77515f2d6498097b27de349ce7bacd6ccd7d272
1terator <kovalexander13@gmail.com>
2022-12-08 01:58:31 +0300

    update

--- newton/service.py
+++ newton/service.py
@@ -1,13 +1,14 @@
 from typing import Any
+import random
 
-from sympy import diff, Symbol, sympify, Matrix
+from sympy import diff, Symbol, sympify, Matrix, symbols
 
 
 class Newton:
-    def __init__(self, equations: list[str], vector: list[float], variables: list[Symbol], eps: float):
+    def __init__(self, equations: list[str], variables: str, eps: float, split_str: str = ", "):
         self.eps: float = eps
-        self.vector: list[float] = vector
-        self.variables: list[Symbol] = variables
+        self.vector: Matrix = Matrix([random.uniform(0.1, 20.0) for i in range(len(equations))])
+        self.variables: list[Symbol] = symbols(list(map(str, variables.split(split_str))))
         self.equations: list[str] = equations
 
     def _get_jacobi_matrix(self) -> list[list[str]]:
@@ -15,7 +16,6 @@ class Newton:
 
         for equation in self.equations:
             equation_j = []
-
             for variable in self.variables:
                 equation_j.append(str(diff(equation, variable)))
 
@@ -23,28 +23,64 @@ class Newton:
 
         return jacobi
 
-    def _calculate_matrix(self, matrix: Any) -> list[list[float]]:
+    def _get_local_vars(self) -> dict:
         local_vars = dict()
+
         for i in range(len(self.variables)):
             local_vars[self.variables[i].name] = self.vector[i]
 
+        return local_vars
+
+    def _calculate_matrix(self, matrix: Any) -> list[list[float]]:
+        local_vars = self._get_local_vars()
+        result = []
+
         for i in range(len(matrix)):
+            result_i = []
             for j in range(len(matrix[i])):
-                matrix[i][j] = sympify(matrix[i][j], locals=local_vars)
-
-        return matrix
-
-    def _get_negative_vector(self) -> Any:
-        result = list()
-        for i in self.vector:
-            result.append(-i)
+                result_i.append(sympify(matrix[i][j], locals=local_vars))
+            result.append(result_i)
 
         return result
 
-    def calculate(self):
+    def _calculate_f_matrix(self) -> Matrix:
+        local_vars = self._get_local_vars()
+        result = []
+
+        for equation in self.equations:
+            result.append(sympify(equation, locals=local_vars))
+
+        return Matrix(result)
+
+    @staticmethod
+    def _sum_magnitudes(matrix: Matrix) -> float:
+        result: float = 0.0
+        minus_count = 0
+
+        for value in matrix:
+            if value < 0:
+                minus_count += 1
+            result += abs(value)
+
+        return result if minus_count % 2 == 0 else -result
+
+    def calculate(self) -> Matrix:
         jacobi = self._get_jacobi_matrix()
-        inverse_matrix = Matrix(self._calculate_matrix(jacobi)).inv()
-        f_matrix = Matrix(self._get_negative_vector())
-        y_matrix = inverse_matrix*f_matrix
-        x_matrix = Matrix(self.vector)
-        print(y_matrix+x_matrix)
+        result = {}
+
+        x_last = None
+        delta: float = 1.0
+
+        while not x_last or abs(delta) > self.eps:
+            x_last = self.vector
+            w = Matrix(self._calculate_matrix(jacobi))
+
+            self.vector -= w.inv() * self._calculate_f_matrix()
+
+            delta = self._sum_magnitudes(self.vector - x_last)
+
+        for i in range(len(self.vector)):
+            result[self.variables[i]] = round(self.vector[i], 6)
+
+        return result
+
--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 from newton.service import Newton
-from sympy import diff, symbols, Symbol
 
 
 data = [
-    "E**(x1*x2)+x1**2+x2-1.20+cos(x1)",
-    "x1**2+x2**2+x1-0.55"
+    "x+x*y**3-9",
+    "x*y+x*y**2-6"
 ]
-vector = [0.6, 0.5]
-variables_str = "x1, x2"
-variables = symbols(list(map(str, variables_str.split(", "))))
+variables = "x, y"
 
-newton = Newton(equations=data, vector=vector, variables=variables, eps=0.0000001)
-newton.calculate()
+newton = Newton(equations=data, variables=variables, eps=0.0000001)
+print(newton.calculate())
